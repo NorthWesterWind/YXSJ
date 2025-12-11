9fef86fc5f137c0dcc0dc15e20176fd08f6f1a92
NorthWesterWind <3204737871@qq.com>
2025-12-11 18:36:10 +0800

    commit

---
+++ Assets/Scripts/View/OrderFunction/OrderFunctionView.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using Utils;
+
+namespace View.OrderFunction
+{
+    public class OrderFunctionView : BaseView
+    {
+        public UIButton closeBtn;
+        public TextMeshProUGUI goldtxt;
+        public TextMeshProUGUI  slivertxt;
+        
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
